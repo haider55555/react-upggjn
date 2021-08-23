6375fe01e7841d83ad1b63218aff7703b99791f9
haider55555 <haider.allen@outlook.com>
2021-08-23 17:59:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-upggjn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-upggjn)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className ="container">
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
